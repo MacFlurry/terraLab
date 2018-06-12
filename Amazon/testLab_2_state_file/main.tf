569b7c3b75783e698335fafb6b834fd48da62508
MacFlurry <omegabk@gmail.com>
2018-06-12 23:14:34 +0200

    s3_bucket_remote_state

--- Amazon/testLab_2_state_file/main.tf
+++ Amazon/testLab_2_state_file/main.tf
@@ -2,6 +2,16 @@ provider "aws" {
    region = "eu-west-1"
 }
 
+data "terraform_remote_state" "myBackend" {
+   backend = "s3"
+   config {
+       bucket = "terraform-bucket-omega"
+       key = "global/s3/terraform.tfstate"
+       region = "eu-west-1"
+       encrypt = true
+    }
+}
+
 # Create an S3 bucket by using aws_s3_bucket
 
 resource "aws_s3_bucket" "terraform_state" {
@@ -14,4 +24,8 @@ resource "aws_s3_bucket" "terraform_state" {
     lifecycle {
         prevent_destroy = true
     }
+}
+
+output "s3_bucket_arn" {
+    value = "${aws_s3_bucket.terraform_state.arn}"
 }
